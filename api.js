ed358bb4cdcee1cbe79ee4d1b3839c3aa2dc99a9
Brooky <j_sena1@u.pacific.edu>
2024-02-13 20:20:02 -0800

    added api page and improt to html file

---
+++ api.js
@@ -0,0 +1,6 @@
+const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+fetch('https://www.virustotal.com/api/v3/urls/id', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
