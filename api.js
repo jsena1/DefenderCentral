649261cd78842c1ea97f67178926578c8634c2a8
Brooky <j_sena1@u.pacific.edu>
2024-02-16 13:25:46 -0800

    added scan into a function

--- api.js
+++ api.js
@@ -1,6 +1,12 @@
-const options = {method: 'GET', headers: {accept: 'application/json'}};
 
-fetch('https://www.virustotal.com/api/v3/urls/id', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+function scan(){
+  //
+  const urlToCheck= 
+  const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+  fetch('https://www.virustotal.com/api/v3/urls/id', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+};
